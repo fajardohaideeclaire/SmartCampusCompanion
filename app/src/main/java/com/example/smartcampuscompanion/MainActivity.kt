5d425c0ac8c8e4a1599ec3f9ffdf94710d9d2459
Haidee Claire Fajardo <fajardohaideeclaire@gmail.com>
2026-02-01 11:23:07 +0800

    Wire theme and navigation to MainActivity

--- app/src/main/java/com/example/smartcampuscompanion/MainActivity.kt
+++ app/src/main/java/com/example/smartcampuscompanion/MainActivity.kt
@@ -3,45 +3,17 @@ package com.example.smartcampuscompanion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.smartcampuscompanion.ui.theme.SmartCampusCompanionTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.smartcampuscompanion.ui.theme.SmartCampusTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            SmartCampusCompanionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            SmartCampusTheme {
+                val navController = rememberNavController()
+                AppNav(navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartCampusCompanionTheme {
-        Greeting("Android")
     }
 }
