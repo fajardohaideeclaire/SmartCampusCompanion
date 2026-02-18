5369a37eeb784e8373b8a7b4973653e55dda5c44
Haidee Claire Fajardo <fajardohaideeclaire@gmail.com>
2026-02-18 22:05:56 +0800

    Added missing imports

--- app/src/main/java/com/example/smartcampuscompanion/AppNav.kt
+++ app/src/main/java/com/example/smartcampuscompanion/AppNav.kt
@@ -4,6 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.compose.ui.platform.LocalContext
+import com.example.smartcampuscompanion.data.local.AppDatabase
+import com.example.smartcampuscompanion.data.repository.TaskRepository
+import com.example.smartcampuscompanion.data.repository.AnnouncementRepository
+import com.example.smartcampuscompanion.viewmodel.TaskViewModel
+import com.example.smartcampuscompanion.viewmodel.AnnouncementViewModel
+import com.example.smartcampuscompanion.ui.task.TaskScreen
+import com.example.smartcampuscompanion.ui.announcement.AnnouncementScreen
+
 
 @Composable
 fun AppNav(navController: NavHostController) {
