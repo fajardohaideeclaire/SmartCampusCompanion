a5557b61dce5a1519b3cf623a0af6c9a58fd9917
DAVE173234 <frenzdavedacillo@gmail.com>
2026-02-18 19:48:16 +0800

    Create TaskEntity for Room database

--- app/src/main/java/com/example/smartcampuscompanion/data/local/TaskEntity.kt
+++ app/src/main/java/com/example/smartcampuscompanion/data/local/TaskEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "tasks")
-data class `TaskEntity.kt`(
+data class TaskEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
